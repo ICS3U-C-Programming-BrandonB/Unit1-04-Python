c1cda9193d9ec6625313c4d6e88afb2d3a7f840a
Brandon Brandhuber <brandon.brandhuber@stu.ocsb.ca>
2025-09-16 17:18:29 +0000

    basic math

---
+++ basic_math.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Created By: Brandon
+# Date: September 16th, 2025
+# This program displays basic math.
+
+
+def main():
+    print("6-2={}".format(6 - 2))  # subtraction
+    print("8+6={}".format(8 + 6))  # addition
+    print("7*5={}".format(7 * 5))  # multiplication
+    print("8/4={}".format(8 / 4))  # division
+    print("8/5={}".format(8 / 5))  # division
+    print("7+4*2={}".format(7 + 4 * 2))  # addition and multiplication
+    print("3^4={}".format(3 ^ 4))  # bitwise operator
+    print("4**5={}".format(4**5))  # exponent
+    print("12**0.25={}".format(12**0.25))  # square root
+
+
+if __name__ == "__main__":
+    main()
